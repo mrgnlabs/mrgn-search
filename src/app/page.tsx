637ddfe663c96d697baf1d3ac411f6e6c7bef501
chambaz <adammchambers@gmail.com>
2025-02-11 19:08:43 -0500

    feat: basic search form styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
-import { Input } from "@/components/ui/input";
+import { SearchInput } from "@/components/ui/search-input";
 import { Button } from "@/components/ui/button";
 
 export default function AccountsSearchPage() {
   return (
-    <div className="flex flex-col items-center gap-8 py-8">
-      <p>Search for marginfi accounts</p>
-      <form className="flex gap-2">
-        <Input placeholder="Search by wallet address..." />
-        <Button>Search</Button>
+    <div className="mx-auto flex max-w-2xl flex-col items-center gap-8 pb-8 pt-16">
+      <p className="text-2xl">Search for marginfi accounts</p>
+      <form className="flex w-full gap-2">
+        <SearchInput placeholder="Search by wallet address..." />
+        <Button size="lg">Search</Button>
       </form>
     </div>
   );
