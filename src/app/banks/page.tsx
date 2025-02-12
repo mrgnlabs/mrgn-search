ccd4d9ebd588e67b1f5343eae511bbc116f7145e
chambaz <adammchambers@gmail.com>
2025-02-11 19:15:30 -0500

    feat: header / page styling

--- src/app/banks/page.tsx
+++ src/app/banks/page.tsx
@@ -1,10 +1,17 @@
+import { Metadata } from "next";
+
 import { SearchInput } from "@/components/ui/search-input";
 import { Button } from "@/components/ui/button";
+
+export const metadata: Metadata = {
+  title: "Marginfi Search - Banks",
+  description: "Search for marginfi banks",
+};
 
 export default function BanksSearchPage() {
   return (
     <div className="mx-auto flex max-w-2xl flex-col items-center gap-8 pb-8 pt-16">
-      <p className="text-2xl">Search for marginfi banks</p>
+      <h1 className="text-3xl">Search for marginfi banks</h1>
       <form className="flex w-full gap-2">
         <SearchInput placeholder="Search by bank address or symbol (e.g USDC)..." />
         <Button size="lg">Search</Button>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
+import { Metadata } from "next";
+
 import { SearchInput } from "@/components/ui/search-input";
 import { Button } from "@/components/ui/button";
+
+export const metadata: Metadata = {
+  title: "Marginfi Search - Accounts",
+  description: "Search for marginfi accounts",
+};
 
 export default function AccountsSearchPage() {
   return (
     <div className="mx-auto flex max-w-2xl flex-col items-center gap-8 pb-8 pt-16">
-      <p className="text-2xl">Search for marginfi accounts</p>
+      <h1 className="text-3xl">Search for marginfi accounts</h1>
       <form className="flex w-full gap-2">
         <SearchInput placeholder="Search by wallet address..." />
         <Button size="lg">Search</Button>
