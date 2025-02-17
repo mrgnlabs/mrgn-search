a4f5bab0030e97613f8682c068c0a610ac995f2c
chambaz <adammchambers@gmail.com>
2025-02-17 07:55:26 -0500

    feat: mobile mobile

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,9 +3,47 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
-import { IconBrandGithub } from "@tabler/icons-react";
+import {
+  IconAddressBook,
+  IconBuildingBank,
+  IconBrandGithub,
+  IconUser,
+  IconMenu2,
+} from "@tabler/icons-react";
 
 import { Button } from "@/components/ui/button";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { cn } from "@/lib/utils";
+
+const links = [
+  {
+    href: "/",
+    label: "Search accounts",
+    icon: <IconUser />,
+  },
+  {
+    href: "/banks",
+    label: "Search banks",
+    icon: <IconBuildingBank />,
+  },
+  {
+    href: "/addresses",
+    label: "Common addresses",
+    icon: <IconAddressBook />,
+  },
+  {
+    href: "https://github.com/mrgnlabs/marginfi-search",
+    label: "Github",
+    icon: <IconBrandGithub />,
+  },
+];
 
 const Header = () => {
   const pathname = usePathname();
@@ -26,40 +64,53 @@ const Header = () => {
         </Link>
       </h1>
       <nav className="hidden w-full items-center gap-2 md:flex">
-        <Link href="/">
-          <Button
-            variant="ghost"
-            className={pathname === "/" ? "bg-muted" : ""}
+        {links.map((link, index) => (
+          <Link
+            href={link.href}
+            key={index}
+            className={cn(index === links.length - 1 && "ml-auto")}
           >
-            Accounts
-          </Button>
-        </Link>
-        <Link href="/banks">
-          <Button
-            variant="ghost"
-            className={pathname === "/banks" ? "bg-muted" : ""}
-          >
-            Banks
-          </Button>
-        </Link>
-        <Link href="/addresses">
-          <Button
-            variant="ghost"
-            className={pathname === "/addresses" ? "bg-muted" : ""}
-          >
-            Addresses
-          </Button>
-        </Link>
-        <Link
-          href="https://github.com/mrgnlabs/marginfi-search"
-          className="ml-auto flex"
-        >
-          <Button variant="secondary">
-            <IconBrandGithub />
-            Github
-          </Button>
-        </Link>
+            <Button
+              variant="ghost"
+              className={cn(pathname === link.href && "bg-muted")}
+            >
+              {link.label === "Github" && link.icon}
+              {link.label}
+            </Button>
+          </Link>
+        ))}
       </nav>
+      <Sheet>
+        <SheetTrigger asChild>
+          <Button variant="outline" className="ml-auto md:hidden">
+            <IconMenu2 />
+          </Button>
+        </SheetTrigger>
+        <SheetContent>
+          <SheetHeader className="sr-only">
+            <SheetTitle>Marginfi Search</SheetTitle>
+            <SheetDescription>
+              Search for marginfi accounts, banks, and addresses.
+            </SheetDescription>
+          </SheetHeader>
+          <div className="mt-8 grid gap-2 py-4">
+            {links.map((link, index) => (
+              <Link href={link.href} key={index} className="w-full">
+                <Button
+                  variant="ghost"
+                  className={cn(
+                    "w-full justify-start",
+                    pathname === link.href && "bg-muted",
+                  )}
+                >
+                  {link.icon}
+                  {link.label}
+                </Button>
+              </Link>
+            ))}
+          </div>
+        </SheetContent>
+      </Sheet>
     </header>
   );
 };
