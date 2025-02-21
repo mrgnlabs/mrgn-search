2a50e85f885912ea5323f08633310b19f9da3d4d
chambaz <adammchambers@gmail.com>
2025-02-21 13:47:28 -0500

    feat: export individual addresses and add group address to bank search

--- src/lib/consts.ts
+++ src/lib/consts.ts
@@ -1,29 +1,39 @@
 export const STAKED_BANK_METADATA_URL =
   "https://storage.googleapis.com/mrgn-public/mrgn-staked-bank-metadata-cache.json";
 
+export const MARGINFI_PROGRAM_ID =
+  "MFv2hWf31Z9kbCa1snEPYctwafyhdvnV7FZnsebVacA";
+export const MARGINFI_STAGING_PROGRAM_ID =
+  "stag8sTKds2h4KzjUw3zKTsxbqvT4XKHdaR9X9E6Rct";
+export const MARGINFI_MAIN_GROUP_ID =
+  "4qp6Fx6tnZkY5Wropq9wUYgtFxXKwE6viZxFHg3rdAG8";
+export const MARGINFI_STAGING_GROUP_ID =
+  "FCPfpHA69EbS8f9KKSreTRkXbzFpunsKuYf5qNmnJjpo";
+export const MARGINFI_LOOKUP_TABLES_ID = [
+  "HGmknUTUmeovMc9ryERNWG6UFZDFDVr9xrum3ZhyL4fC",
+  "5FuKF7C1tJji2mXZuJ14U9oDb37is5mmvYLf4KwojoF1",
+  "FEFhAFKz48P3w82Ds5VhvyEDwhRqu2FejmnuxEPZ8wNR",
+];
+
 export const ADDRESSES = [
   {
     description: "marginfi-v2 program",
-    publicKey: "MFv2hWf31Z9kbCa1snEPYctwafyhdvnV7FZnsebVacA",
+    publicKey: MARGINFI_PROGRAM_ID,
   },
   {
     description: "marginfi-v2 staging program",
-    publicKey: "stag8sTKds2h4KzjUw3zKTsxbqvT4XKHdaR9X9E6Rct",
+    publicKey: MARGINFI_STAGING_PROGRAM_ID,
   },
   {
     description: "marginfi-v2 main group",
-    publicKey: "4qp6Fx6tnZkY5Wropq9wUYgtFxXKwE6viZxFHg3rdAG8",
+    publicKey: MARGINFI_MAIN_GROUP_ID,
   },
   {
     description: "marginfi-v2 staging group",
-    publicKey: "FCPfpHA69EbS8f9KKSreTRkXbzFpunsKuYf5qNmnJjpo",
+    publicKey: MARGINFI_STAGING_GROUP_ID,
   },
   {
     description: "marginfi lookup tables",
-    publicKey: [
-      "HGmknUTUmeovMc9ryERNWG6UFZDFDVr9xrum3ZhyL4fC",
-      "5FuKF7C1tJji2mXZuJ14U9oDb37is5mmvYLf4KwojoF1",
-      "FEFhAFKz48P3w82Ds5VhvyEDwhRqu2FejmnuxEPZ8wNR",
-    ],
+    publicKey: MARGINFI_LOOKUP_TABLES_ID,
   },
 ];
